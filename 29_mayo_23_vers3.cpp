e0918be7b1224262d524aae0818c75ac4bceb1de
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-05-29 07:59:54 -0600

    Add files via upload

---
+++ 29_mayo_23_vers3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int a, b, c;
+int main()
+{
+	int valor ;
+	printf("Tres valores: \n ");
+	scanf("%d %d %d\a",&a,&b,&c);
+	valor= a+b+c;
+	printf("valor de a: %d\n",a);
+	printf("valor de b: %d\n",b);
+	printf("valor de c: %d\n",c);
+	printf("La sima de las variables: %d", valor);
+	return 0;
+}
