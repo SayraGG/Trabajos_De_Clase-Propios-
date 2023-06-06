7fb9db189aa028b2becc8d5bf89267948cd9465d
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-06-06 09:43:31 -0600

    Add files via upload

---
+++ 6_jun_23_ej1.cpp
@@ -0,0 +1,12 @@
+/* programa numero: 1
+Titulo: Comportamiento o funcionamiento de una sentencia(bucle) while
+fecha: 6/junio/2023
+Nombre: Sayra González García 
+*/
+#include<stdio.h>
+int main()
+{
+	int x=0;
+	while (x<10)
+	printf("X:%d\n",x++);
+}
---
+++ 6_jun_23_ej2.cpp
@@ -0,0 +1,16 @@
+/* programa numero: 2
+Titulo: Comportamiento o funcionamiento de una sentencia(bucle) while
+fecha: 6/junio/2023
+Nombre: Sayra González García 
+Funcionamiento: El programa
+*/
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while(contador<100)
+	{
+		printf("%d\n", contador);
+		contador--;
+	}
+}
