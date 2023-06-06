7d52a6d18f15f6b64390ec31c1f94e7d68ebde7f
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-06-06 10:11:00 -0600

    Add files via upload

---
+++ 6_jun_23_ej4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while(contador>100)
+	{
+		printf("%d\n", contador);//si la prueba logica es falsa no se ejecuta la sentencia de instrucciones
+		contador++;
+	}
+}
