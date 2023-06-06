ca5077eb11dde3da59c9d5eaf23c46e3878b5c9b
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-06-06 10:43:26 -0600

    Add files via upload

---
+++ 6_jun_23_ej7.cpp
@@ -0,0 +1,13 @@
+//el programa sin llaves pone de manera infinita el numero 1
+// con llaves cuenta del 1 al 24
+#include<stdio.h>
+int main()
+{
+	int contador=1;
+	while (contador<25)
+	{
+	printf("%d\n", contador);
+	contador++;
+    }
+	return 0;
+}
