a7140c3368a7e7a00d21de2e34bc871d0ee0efd6
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-06-09 14:17:01 -0600

    9_jun_23_ej1.cpp

--- 9_jun_23_ej1.cpp
+++ 9_jun_23_ej1.cpp
@@ -1,8 +1,9 @@
 #include<stdio.h>
+// no tenia internet por eso los subi terde los de esta  fecha
 int main()
 {
 	int fila,blancos, cuenta_as;
 	puts(" ");
 	for (;;)
-	printf("Siempre así, te llamamos siempre así "); //un bucle infinito termina en ctrl+c
+	printf("Siempre asÃ­, te llamamos siempre asÃ­ "); //un bucle infinito termina en ctrl+c
 }
