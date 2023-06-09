446dc8f9d9c977a69433ad13cb27259e6c651bb4
Sayra González <132223965+SayraGG@users.noreply.github.com>
2023-06-09 14:42:16 -0600

    Ejemplo burbuja.cpp

--- ejemplo burbuja.cpp
+++ ejemplo burbuja.cpp
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#define TAM 6
 
-int main(){
-
-int vector [10]={10,2,3,35,4,7,9,11,44,6};
-
-int aux;
-
-for(int i = 0; i < 10; i++){
-
-for(int j = 0; j < 10; j++){
-
-if (vector[j] > vector[j+1]);
+int main()
 {
 
-aux =vector[j+1];
- vector[j+1] = vector [j]; 
-vector[j] = aux;
- }
- }
- }
-for(int i = 0; i < 10; i++){ printf("%d\t",vector[i]);
+int lista[TAM]={12,10,5,6,1,3};	 //Declaracion e Inicializacion de un array
+int temp=0; 			 //Variable temporal
+int i,j;			 //variables corredoras del ciclo
+
+printf("La lista DESORDENADA es: \n");
+
+for (i=0;i<TAM;i++)
+   printf("%3d",lista[i]);	//impresion de la lista con espacio de 3 lineas (%3d)
+
+for (i=1;i<TAM;i++)
+{
+	for (j=0;j<TAM-1;j++)
+	{
+		if (lista[j] > lista[j+1])	 //condicion
+		{
+			temp = lista[j];	 //temp guarda momentaneamente el valor de lista[j]
+			lista[j]=lista[j+1];  //Asigno al la posicion lista[j], lo que hay en lista[j+1]
+			lista[j+1]=temp;	//obtendra un nuevo valor por parte de temp.
+		}
+	}
+
 }
+
+printf("\nLos valores ORDENADOS de lista son: \n");
+for(i=0;i<TAM;i++)
+    printf("%3d",lista[i]);
+
+return 0;
 }
